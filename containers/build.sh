26560203178e0b22e16567d7ef49dce20c144a49
CLX <twbrandon7@gmail.com>
2025-01-26 08:21:17 +0000

    feat: parameterize Docker image name in build script

--- containers/build.sh
+++ containers/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+USERNAME="twbrandon7"
+IMAGE_NAME="pytorch-dev-container"
 TAG="torch-2.5.1-cu124-py3.11"
+
 docker build \
     --build-arg PYTORCH_VERSION=2.5.1 \
     --build-arg TORCHVISION_VERSION=0.20.1 \
     --build-arg TORCHAUDIO_VERSION=2.5.1 \
     --build-arg PYTORCH_INDEX_URL=https://download.pytorch.org/whl/cu124 \
     --build-arg PYTHON_VERSION=3.11 \
-    -t "twbrandon7/pytorch-dev-container:$TAG" .
+    -t "$USERNAME/$IMAGE_NAME:$TAG" .
