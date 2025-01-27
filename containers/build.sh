a345a04b1c8d3319ba04439e4fe6c878f6bb974a
CLX <twbrandon7@gmail.com>
2025-01-27 07:06:39 +0000

    refactor: use poetry to manage global packages

--- containers/build.sh
+++ containers/build.sh
@@ -4,9 +4,5 @@ IMAGE_NAME="pytorch-dev-container"
 TAG="torch-2.5.1-cu124-py3.11"
 
 docker build \
-    --build-arg PYTORCH_VERSION=2.5.1 \
-    --build-arg TORCHVISION_VERSION=0.20.1 \
-    --build-arg TORCHAUDIO_VERSION=2.5.1 \
-    --build-arg PYTORCH_INDEX_URL=https://download.pytorch.org/whl/cu124 \
     --build-arg PYTHON_VERSION=3.11 \
     -t "$USERNAME/$IMAGE_NAME:$TAG" .
